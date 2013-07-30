b2515e655e6effbc1b9a81e86504e0ba78c8acf9
Ilya Batiy <ilya@ostrovok.ru>
2013-07-31 00:57:21 +0400

    * fix find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
@@ -8,7 +8,12 @@ __version__ = '0.0.1'
 setup(
     name='butler',
     url='http://www.github.com/pod2metra/butler/',
-    packages=('butler',),
+    packages=find_packages(
+        exclude=(
+            'test_project',
+            'tests',
+        )
+    ),
     version='v0.1.0',
     include_package_data=True,
     install_requires=[REQUIREMENTS],
