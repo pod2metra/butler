9abacaee505689fe7db46dfa68bf26cd24a3e87f
Ilya Batiy <ilya@ostrovok.ru>
2013-07-31 00:51:49 +0400

    * added nose to apps

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -116,7 +116,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'butler'
+    'butler',
+    'django_nose',
 )
 
 # A sample logging configuration. The only tangible logging
